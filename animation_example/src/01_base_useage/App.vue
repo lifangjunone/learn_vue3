75ab0a8d4b1115c7897fd3a02a3cdce93d5fe39d
李芳军 <lifangjun@lifangjundeMacBook-Pro.local>
2023-01-19 11:32:10 +0800

    feat(component): slot and dynamic component

---
+++ animation_example/src/01_base_useage/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <h2 v-if="isShow ">Hello World</h2> 
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isShow: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ animation_example/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue"
+import App from "./01_base_useage/App.vue"
+
+createApp(App).mount("#app")
